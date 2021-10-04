f3f29dcb596e1e1d57226248deea8ae5bc28ef63
Fatih Topcu <fthtpu@gmail.com>
2021-10-04 10:58:46 +0300

    d

--- ConsoleProgramlama/Program.cs
+++ ConsoleProgramlama/Program.cs
@@ -4,9 +4,18 @@ namespace ConsoleProgramlama
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Isminizi Girin :");
+            string name = Console.ReadLine();
+
+
+            Console.WriteLine("Soyisminizi Girin");
+            string surname =Console.ReadLine();
+
+            Console.WriteLine("Merhaba  " + name + " " + surname);
+            
         }
     }
 }
